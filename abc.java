a675a8b3f9dbba2f1b8410888146c2d17ae0ddc0
SewardHe <rainee@qq.com>
2020-01-08 16:39:21 +0800

    test 4

--- abc.java
+++ abc.java
@@ -1,10 +1,19 @@
+import java.util.jar.Attributes.Name;
+
 /**
  * abc
  */
 public class abc {
 
-    public void name() {
+    public static void main(String[] args) {
+        abc.name();
+    }
+
+    public static void name() {
         String test = "abc";
         int age = 18;
+
+        System.out.println("name:" + test);
+        System.out.println("age:" + age);
     }
 }
